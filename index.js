e337db5218592a7c1cfaa68fdbb6bdd1df63ea81
manneanjaneyulu <manne.anjaneyulu@successive.tech>
2021-01-12 20:27:11 +0530

    heap-dump-added1

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const port = process.env.PORT || 3000;
 require("newrelic");
 var lt = require( 'load-test' );
 var heapdump = require('heapdump');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
+});
+
+heapdump.writeSnapshot(function(err, filename) {
+  console.log('dump written to', filename);
 });
 
 server.listen(port, () => {
